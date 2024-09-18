d0440575fdd8c85ce0a8b9ffd9ec7cbc63a36f52
mdshohaan <irfanulislamdu@gmail.com>
2024-09-19 00:55:57 +0600

    second commit

--- login.js
+++ login.js
@@ -1,5 +1,16 @@
 document.getElementById('btnLogIn').addEventListener('click',function(event){
     event.preventDefault();
     const phoneNumber= document.getElementById('phone_number').value
+  const pinNumber =document.getElementById('pin_number').value 
 
+
+//   console.log(phoneNumber,pinNumber);
+if(phoneNumber === '5' && pinNumber === '1234'){
+
+    console.log('logged');
+    window.location.href = '/home.html'
+}
+else{
+    alert('wrong')
+}
 })
