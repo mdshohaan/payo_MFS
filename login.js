b3fb85a8d9fe640489e59dd6d9d30d6a4469d7c6
mdshohaan <irfanulislamdu@gmail.com>
2024-09-18 21:39:48 +0600

    first commit

---
+++ login.js
@@ -0,0 +1,5 @@
+document.getElementById('btnLogIn').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber= document.getElementById('phone_number').value
+
+})
